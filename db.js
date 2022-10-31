be46d37cde4637c54dfcf080ef656ad539ce9510
OldSchoolIzzy <herreraisrael79.ih@gmail.com>
2022-10-31 10:16:49 -0500

    update db file
    Fixed the database connection. we can now connect!

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 /* connects to the database */
 let mysql = require('mysql');
 
-let connection = mysql.createConnection({
+let con = mysql.createConnection({
     host: '45.55.136.114',
     user: 'kingsF2022',
-    password: 'king_r_here!',
+    password: 'kings_r_here!',
     database: 'kingsF2022'
 });
 
-connection.connect(function(err) {
+con.connect(function(err) {
     if (err) throw err;
     console.log('connected!');
 });
 
-module.exports = connection;
+module.exports = con;
