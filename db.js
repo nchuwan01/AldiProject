491ead88ccb0fd8a8cc8d8c7b8de388e522ea46c
Narayan Chuwan01 <nchuwan01@aurora.edu>
2022-11-03 01:54:35 -0500

    Accural Table code
    Code to convert JSON into the table

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 /* connects to the database */
 let mysql = require('mysql');
+const bodyParser = require("body-parser");
+let PTODATA = require('./PTOAccrualBrackets.json');
 
+//console.log(JSON.parse(ObjectT[0].Name));
+
+//console.log(ObjectT.length);
 let con = mysql.createConnection({
     host: '45.55.136.114',
     user: 'kingsF2022',
@@ -8,9 +13,38 @@ let con = mysql.createConnection({
     database: 'kingsF2022'
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log('connected!');
+
+con.connect((err) => {
+    if (err) return console.error(
+        'error: ' + err.message);
+
+    for(let i=0; i<PTODATA.length; i++)
+    {
+            var yearsWorked = PTODATA[i].NumberOfYears,
+                role = PTODATA[i].Role,
+                maxVac = PTODATA[i].MaxVacation,
+                vacPerYear = PTODATA[i].VacationPerYear,
+                vacAccuralDate = PTODATA[i].VacationAccuralDate,
+                maxPer = PTODATA[i].MaxPersonal,
+                perPerYear = PTODATA[i].PersonalPerYear,
+                perAccuralDate= PTODATA[i].PersonalAccuralDate,
+                maxSick = PTODATA[i].MaxSick,
+                sickPerYear = PTODATA[i].SickPerYear,
+                sickAccural = PTODATA[i].SickAccuralDate;
+
+            var insertStatement =
+                `INSERT INTO accural values(?, ?, ?, ?,?,?,?,?,?,?,?)`;
+            var items = [yearsWorked, role, maxVac, vacPerYear,vacAccuralDate,maxPer,perPerYear,perAccuralDate,maxSick,sickPerYear,sickAccural ];
+
+            con.query(insertStatement, items,
+                (err, results, fields) => {
+                        return console.log(err);
+                    }
+                );
+    }
+
+
 });
+
 
 module.exports = con;
