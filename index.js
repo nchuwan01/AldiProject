cd077f754adfc6e039911075a27534827e50c0a9
OldSchoolIzzy <herreraisrael79.ih@gmail.com>
2022-11-16 08:43:09 -0600

    tweaks

--- index.js
+++ index.js
@@ -7,38 +7,47 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static('public'))
 
+app.get("/logIn", function (req, res){
+    res.render("logIn")
+})
 app.get("/managerStatusPage", function (req, res){
-    res.render("managerStatusPage");
+    res.render("ManagerFiles/managerStatusPage");
 })
 app.get("/devStatusPage", function (req, res){
-    res.render("devStatusPage")
+    res.render("DevPugs/devStatusPage")
 })
 app.get("/managerPrintReport", function (req, res){
-    res.render("managerPrintReport")
+    res.render("ManagerFiles/managerPrintReport")
 })
 app.get("/directorPrintReport", function (req, res){
-    res.render("directorPrintReport")
+    res.render("DirectorPages/directorPrintReport")
 })
 app.get("/managerEmployeeReport", function (req, res){
-    res.render("managerEmployeeReport")
+    res.render("ManagerFiles/managerEmployeeReport")
 })
 app.get("/directorEmployeeReport", function (req, res){
-    res.render("directorEmployeeReport")
+    res.render("DirectorPages/directorEmployeeReport")
 })
 app.get("/directorHomePage", function(req, res) {
-    res.render("DirectorHomePage");
+    res.render("DirectorPages/DirectorHomePage");
 })
 app.get("/makeRequest", function(req, res) {
-    res.render("RequestPage");
+    res.render("ManagerFiles/RequestPage");
 })
 app.get("/devmakeRequest", function(req, res) {
-    res.render("devRequestPage");
+    res.render("DevPugs/devRequestPage");
 })
 app.get("/devHomePage", function(req, res) {
-    res.render("devHomePage");
+    res.render("DevPugs/devHomePage");
 })
 app.get("/managerHomePage", function(req, res) {
-    res.render("managerHomePage");
+    res.render("ManagerFiles/managerHomePage");
+})
+app.get("/resetPassword", function (req, res){
+    res.render("resetPassword")
+})
+app.get("/registrationPage", function (req, res){
+    res.render("registrationPage")
 })
 let port = 3019;
 app.listen(port, ()=>{
