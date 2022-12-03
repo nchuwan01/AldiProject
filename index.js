63098bcc5a289b2ba48f16c9550c27baed1ee499
Narayan Chuwan01 <nchuwan01@aurora.edu>
2022-12-03 09:48:41 -0600

    changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 let bodyParser = require('body-parser')
-let con = require('./public/js/db')
 app.set('view engine', 'pug');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -11,18 +10,6 @@ const session = require('express-session');
 const path = require('path');
 const mysql = require('mysql');
 let connection = require ("./dbLogin");
-let username ="";
-let name="";
-let date = new Date();
-let day = date.getDate();
-let month = date.getMonth()+1;
-let year = date.getFullYear();
-let hours= date.getHours();
-let minutes=date.getMinutes();
-let seconds= date.getSeconds();
-let mdate=`${year}-${month}-${day}`
-let fullDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
-
 app.use(session({
     secret: 'secret',
     resave: true,
@@ -38,7 +25,7 @@ app.get('/', function(req, res) {
 
 
 app.post('/auth',function (req,res){
-    username = req.body.username
+    let username = req.body.username
     let password = req.body.password
     console.log(username,password)
     if(username && password){
@@ -51,6 +38,8 @@ app.post('/auth',function (req,res){
                 req.session.loggedin = true;
                 req.session.username = username;
 
+
+
                 // Redirect to home page
                 connection.query('SELECT role FROM employee WHERE employeeid = ?',[req.body.username],function(error,results,fields){
                     console.log(results);
@@ -60,16 +49,13 @@ app.post('/auth',function (req,res){
                     console.log(json[0].role);
                     if (error) throw error;
                     if(json[0].role==="Employee"){
-                        //res.render("DevPugs/devHomePage")
-                        res.redirect("/devHomePage")
+                        res.render("DevPugs/devHomePage")
                         res.end();
                     }else if(json[0].role == "Manager"){
-                        //res.render("ManagerFiles/managerHomePage")
-                        res.redirect("/managerHomePage")
+                        res.render("ManagerFiles/managerHomePage")
                         res.end();
                     }else{
-                        //res.render("DirectorPages/DirectorHomePage")
-                        res.redirect("/directorHomePage")
+                        res.render("DirectorPages/DirectorHomePage")
                     }});
             }  else {
                 res.render("login", {data: "Incorrect Employee ID/Password"})
@@ -87,20 +73,10 @@ app.post('/auth',function (req,res){
 });
 
 app.get("/managerStatusPage", function (req, res){
-    let sql = `SELECT * FROM request where employeeid = 817052`
-    con.query(sql, function (error, results){
-        res.render("ManagerFiles/managerStatusPage", {
-            rows : results
-        })
-    })
+    res.render("ManagerFiles/managerStatusPage");
 })
 app.get("/devStatusPage", function (req, res){
-    let sql = `SELECT * FROM request where employeeid = 936350`
-    con.query(sql, function (error, results){
-        res.render("DevPugs/devStatusPage", {
-            rows : results
-        })
-    })
+    res.render("DevPugs/devStatusPage")
 })
 app.get("/managerPrintReport", function (req, res){
     res.render("ManagerFiles/managerPrintReport")
@@ -115,23 +91,8 @@ app.get("/directorEmployeeReport", function (req, res){
     res.render("DirectorPages/directorEmployeeReport")
 })
 app.get("/directorHomePage", function(req, res) {
-    if(req.session.loggedin){
-        connection.query('SELECT firstName FROM employee WHERE employeeid = ?',[username],function(error,results,fields){
-            console.log(results);
-            var string = JSON.stringify(results);
-            console.log('>> string: ', string);
-            var json = JSON.parse(string);
-            console.log(json[0].firstName);
-            name =  json[0].firstName;
-            console.log(fullDate);
-            if (error) throw error;
-            res.render("DirectorPages/DirectorHomePage",{
-                user: username,
-                date:mdate,
-                empname: name});
-        })}else {
-        res.send('Please login to view this page!');
-    }})
+    res.render("DirectorPages/DirectorHomePage");
+})
 app.get("/makeRequest", function(req, res) {
     res.render("ManagerFiles/RequestPage");
 })
@@ -139,45 +100,11 @@ app.get("/devmakeRequest", function(req, res) {
     res.render("DevPugs/devRequestPage");
 })
 app.get("/devHomePage", function(req, res) {
-    if(req.session.loggedin){
-        connection.query('SELECT firstName FROM employee WHERE employeeid = ?',[username],function(error,results,fields){
-            console.log(results);
-            var string = JSON.stringify(results);
-            console.log('>> string: ', string);
-            var json = JSON.parse(string);
-            console.log(json[0].firstName);
-            name =  json[0].firstName;
-            console.log(fullDate);
-            if (error) throw error;
-            res.render("DevPugs/devHomePage",{
-                user: username,
-                date:mdate,
-                empname: name});
-        })}else{
-        res.send('Please login to view this page!');
-    }
-});
+    res.render("DevPugs/devHomePage");
+})
 app.get("/managerHomePage", function(req, res) {
-    if(req.session.loggedin){
-    connection.query('SELECT firstName FROM employee WHERE employeeid = ?',[username],function(error,results,fields){
-        console.log(results);
-        var string = JSON.stringify(results);
-        console.log('>> string: ', string);
-        var json = JSON.parse(string);
-        console.log(json[0].firstName);
-        name =  json[0].firstName;
-        console.log(fullDate);
-        if (error) throw error;
-        res.render("ManagerFiles/managerHomePage",{
-            user: username,
-            date:mdate,
-            empname: name});
-    })}else{
-        res.send('Please login to view this page!');
-    }
-});
-
-
+    res.render("ManagerFiles/managerHomePage");
+})
 app.get("/resetPassword", function (req, res){
     res.render("resetPassword")
 })
