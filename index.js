29b29de55a3c5e2136e43e32a3fcedb65ad6a29c
OldSchoolIzzy <herreraisrael79.ih@gmail.com>
2022-12-05 14:42:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ let bodyParser = require('body-parser')
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 let con = require('./public/js/db')
 >>>>>>> parent of 7b9f71c (Update index.js)
+=======
+let con = require('./public/js/db')
+>>>>>>> parent of bcba4e2 (reset pushed)
 =======
 let con = require('./public/js/db')
 >>>>>>> parent of bcba4e2 (reset pushed)
@@ -156,6 +160,9 @@ app.post('/auth',function (req,res){
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of bcba4e2 (reset pushed)
+=======
 >>>>>>> parent of bcba4e2 (reset pushed)
 =======
 >>>>>>> parent of bcba4e2 (reset pushed)
